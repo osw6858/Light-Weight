5e89311f704d7f993f32525b72cb116754acd141
osw6858 <osw7875@naver.com>
2023-06-22 18:27:28 +0900

    '예외처리추가'

--- src/components/dietComponents/DietIndex.tsx
+++ src/components/dietComponents/DietIndex.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { List, Input, Result, Spin, Button, Modal, Descriptions } from "antd";
-import { QuestionCircleOutlined } from "@ant-design/icons";
+import { QuestionCircleOutlined, WarningOutlined } from "@ant-design/icons";
 import axios from "axios";
 
 export type FoodType = {
@@ -26,7 +26,7 @@ const DietIndex = () => {
   const [loading, setLoading] = useState(false);
   const [foodDtail, setFoodDtail] = useState<FoodType>();
   const [modal2Open, setModal2Open] = useState(false);
-
+  const [error, setError] = useState(false);
   const handleSearchText = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchText(e.target.value);
   };
@@ -34,15 +34,20 @@ const DietIndex = () => {
   const onSearch = (search: string) => {
     setLoading(true); // 로딩 상태 설정
     async function getFood() {
-      const res = await axios.get(FOOD_API_URL + search);
-      const foods: FoodResult = res.data.I2790.row;
-      //  console.log(foods);
-      setData(foods);
-      setLoading(false); // 로딩 상태 해제
-      setHomeScreen(false);
-      if (foods === undefined) {
-        alert("검색결과가 없습니다.");
-        return;
+      try {
+        const res = await axios.get(FOOD_API_URL + search);
+        const foods: FoodResult = res.data.I2790.row;
+        //  console.log(foods);
+        setData(foods);
+        setLoading(false); // 로딩 상태 해제
+        setHomeScreen(false);
+        if (foods === undefined) {
+          alert("검색결과가 없습니다.");
+          return;
+        }
+      } catch (errors) {
+        console.log(errors);
+        setError(true);
       }
     }
     getFood();
@@ -53,90 +58,108 @@ const DietIndex = () => {
     setFoodDtail(item);
   };
 
+  const handleErrorMSG = () => {
+    alert(
+      "음식정보를 받아올 수 없습니다. 네트워크 연결을 확인하거나 관리자에게 문의해 주세요."
+    );
+  };
+
   return (
     <main className="flex flex-col justify-center max-w-screen-sm  mx-auto mt-5 ">
-      <div className="mx-3">
-        <Search
-          placeholder="음식 이름을 입력"
-          enterButton="검색"
-          size="large"
-          onSearch={(e) => onSearch(e)}
-          onChange={handleSearchText}
-          value={searchText}
-          className=" bg-blue-500 rounded-lg "
-        />
-      </div>
-      {homeScreen ? (
-        <Spin spinning={loading} size="large">
-          <Result
-            icon={<QuestionCircleOutlined />}
-            title={<span>운동하려면 영양도 잘 챙기셔야죠!</span>}
-            extra={<span>검색창에 궁금한 음식정보를 검색해 보세요!</span>}
-          />
-        </Spin>
+      {error ? (
+        <button className=" p-5" onClick={handleErrorMSG}>
+          <WarningOutlined className=" text-6xl text-red-500" />
+          <p className=" text-lg py-3 font-bold">
+            음식 정보를 가져오지 못했습니다.
+          </p>
+        </button>
       ) : (
-        <div className="px-6">
-          <Spin spinning={loading} size="large">
-            <List
-              itemLayout="horizontal"
-              dataSource={data}
-              renderItem={(item: FoodType, index) => (
-                <List.Item key={index}>
-                  <List.Item.Meta
-                    title={item.DESC_KOR}
-                    description={`${item.NUTR_CONT1}kcal`}
-                  />
-                  <Button onClick={() => handleOpenModal(item)}>
-                    상세정보
-                  </Button>
-                </List.Item>
-              )}
+        <div>
+          {" "}
+          <div className="mx-3">
+            <Search
+              placeholder="음식 이름을 입력"
+              enterButton="검색"
+              size="large"
+              onSearch={(e) => onSearch(e)}
+              onChange={handleSearchText}
+              value={searchText}
+              className=" bg-blue-500 rounded-lg "
             />
-            <Modal
-              title={foodDtail?.DESC_KOR}
-              centered
-              okType="default"
-              open={modal2Open}
-              onCancel={() => setModal2Open(false)}
-              footer={
-                <Button key="back" onClick={() => setModal2Open(false)}>
-                  닫기
-                </Button>
-              }
-            >
-              <div className="flex flex-col justify-center items-center">
-                <Descriptions>
-                  <Descriptions.Item label="열량">
-                    {foodDtail?.NUTR_CONT1}Kcal
-                  </Descriptions.Item>
-                  <Descriptions.Item label="탄수화물">
-                    {foodDtail?.NUTR_CONT2}g
-                  </Descriptions.Item>
-                  <Descriptions.Item label="단백질">
-                    {foodDtail?.NUTR_CONT3}g
-                  </Descriptions.Item>
-                  <Descriptions.Item label="지방">
-                    {foodDtail?.NUTR_CONT4}g
-                  </Descriptions.Item>
-                  <Descriptions.Item label="당류">
-                    {foodDtail?.NUTR_CONT5}g
-                  </Descriptions.Item>
-                  <Descriptions.Item label="나트륨">
-                    {foodDtail?.NUTR_CONT6}mg
-                  </Descriptions.Item>
-                  <Descriptions.Item label="콜레스트롤">
-                    {foodDtail?.NUTR_CONT7}mg
-                  </Descriptions.Item>
-                  <Descriptions.Item label="포화지방산">
-                    {foodDtail?.NUTR_CONT8}g
-                  </Descriptions.Item>
-                  <Descriptions.Item label="트랜스지방">
-                    {foodDtail?.NUTR_CONT9}g
-                  </Descriptions.Item>
-                </Descriptions>
-              </div>
-            </Modal>
-          </Spin>
+          </div>
+          {homeScreen ? (
+            <Spin spinning={loading} size="large">
+              <Result
+                icon={<QuestionCircleOutlined />}
+                title={<span>운동하려면 영양도 잘 챙기셔야죠!</span>}
+                extra={<span>검색창에 궁금한 음식정보를 검색해 보세요!</span>}
+              />
+            </Spin>
+          ) : (
+            <div className="px-6">
+              <Spin spinning={loading} size="large">
+                <List
+                  itemLayout="horizontal"
+                  dataSource={data}
+                  renderItem={(item: FoodType, index) => (
+                    <List.Item key={index}>
+                      <List.Item.Meta
+                        title={item.DESC_KOR}
+                        description={`${item.NUTR_CONT1}kcal`}
+                      />
+                      <Button onClick={() => handleOpenModal(item)}>
+                        상세정보
+                      </Button>
+                    </List.Item>
+                  )}
+                />
+                <Modal
+                  title={foodDtail?.DESC_KOR}
+                  centered
+                  okType="default"
+                  open={modal2Open}
+                  onCancel={() => setModal2Open(false)}
+                  footer={
+                    <Button key="back" onClick={() => setModal2Open(false)}>
+                      닫기
+                    </Button>
+                  }
+                >
+                  <div className="flex flex-col justify-center items-center">
+                    <Descriptions>
+                      <Descriptions.Item label="열량">
+                        {foodDtail?.NUTR_CONT1}Kcal
+                      </Descriptions.Item>
+                      <Descriptions.Item label="탄수화물">
+                        {foodDtail?.NUTR_CONT2}g
+                      </Descriptions.Item>
+                      <Descriptions.Item label="단백질">
+                        {foodDtail?.NUTR_CONT3}g
+                      </Descriptions.Item>
+                      <Descriptions.Item label="지방">
+                        {foodDtail?.NUTR_CONT4}g
+                      </Descriptions.Item>
+                      <Descriptions.Item label="당류">
+                        {foodDtail?.NUTR_CONT5}g
+                      </Descriptions.Item>
+                      <Descriptions.Item label="나트륨">
+                        {foodDtail?.NUTR_CONT6}mg
+                      </Descriptions.Item>
+                      <Descriptions.Item label="콜레스트롤">
+                        {foodDtail?.NUTR_CONT7}mg
+                      </Descriptions.Item>
+                      <Descriptions.Item label="포화지방산">
+                        {foodDtail?.NUTR_CONT8}g
+                      </Descriptions.Item>
+                      <Descriptions.Item label="트랜스지방">
+                        {foodDtail?.NUTR_CONT9}g
+                      </Descriptions.Item>
+                    </Descriptions>
+                  </div>
+                </Modal>
+              </Spin>
+            </div>
+          )}
         </div>
       )}
     </main>
--- src/components/otherComponents/Weather.tsx
+++ src/components/otherComponents/Weather.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-
+import { WarningOutlined } from "@ant-design/icons";
 const Weather = () => {
   const WEATHER_API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
   const WEATHER_API_URL = import.meta.env.VITE_WEATHER_API_URL;
@@ -8,6 +8,7 @@ const Weather = () => {
   //console.log("api키", WEATHER_API_KEY);
   const [weather, setWeather] = useState<WeatherState>([]);
   const [temp, setTemp] = useState<WeatherMain>({ temp: 0 });
+  const [error, setError] = useState(false);
 
   type WeatherState = Array<{
     id: number;
@@ -29,32 +30,53 @@ const Weather = () => {
         const temp = res.data.main;
         setWeather(weather);
         setTemp(temp);
-      } catch (error) {
-        console.log(error);
+        setError(false);
+      } catch (errors) {
+        console.log(errors);
+        setError(true);
       }
     }
     getWeather();
   }, []);
 
+  const handleErrorMSG = () => {
+    alert(
+      "날씨정보를 받아올 수 없습니다. 네트워크 연결을 확인하거나 관리자에게 문의해 주세요."
+    );
+  };
+
   return (
     <div>
-      {weather.slice(0, 1).map((e, index) => {
-        return (
-          <div
-            key={index}
-            className="flex flex-col  align-middle mx-2 pt-1 md:p-2 md:mr-10"
-          >
-            <img
-              className="object-cover h-12 w-12 md:h-14 md:w-14"
-              src={`https://openweathermap.org/img/wn/${e.icon}@2x.png`}
-              alt="날씨 아이콘"
-            />
-            <span className="text-center text-slate-400 sm:text-base md:text-2xl mx-3">
-              Seoul, {Math.round(temp.temp * 10) / 10}°C
-            </span>
-          </div>
-        );
-      })}
+      {error ? (
+        <button
+          className=" flex justify-center items-center p-5"
+          onClick={handleErrorMSG}
+        >
+          <WarningOutlined className=" text-3xl text-white" />
+          <span className=" text-white text-base px-3">정보 없음</span>
+        </button>
+      ) : (
+        <div>
+          {" "}
+          {weather.slice(0, 1).map((e, index) => {
+            return (
+              <div
+                key={index}
+                className="flex flex-col  align-middle mx-2 pt-1 md:p-2 md:mr-10"
+              >
+                <img
+                  className="object-cover h-12 w-12 md:h-14 md:w-14"
+                  src={`https://openweathermap.org/img/wn/${e.icon}@2x.png`}
+                  alt="날씨 아이콘"
+                />
+                <span className="text-center text-slate-400 sm:text-base md:text-2xl mx-3">
+                  Seoul, {Math.round(temp.temp * 10) / 10}°C
+                </span>
+              </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 };
